dd070bbb251c96efc1bb25b0578634f6187f44ab
dmage20 <dmage20@users.noreply.github.com>
2026-01-02 15:28:52 +0800

    chore: remove insecure SSL verification bypass

--- app/services/ecfr_client.rb
+++ app/services/ecfr_client.rb
@@ -195,7 +195,7 @@ class EcfrClient
   # Streams download directly to a file path
   def download_with_retry(url, destination_path, attempt: 1)
     uri = URI(url)
-    Net::HTTP.start(uri.host, uri.port, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE, open_timeout: 10, read_timeout: 120) do |http|
+    Net::HTTP.start(uri.host, uri.port, use_ssl: true, open_timeout: 10, read_timeout: 120) do |http|
       request = Net::HTTP::Get.new(uri)
 
       http.request(request) do |response|
@@ -227,7 +227,7 @@ class EcfrClient
     # Keeps existing behavior for small JSON headers (fetch_agencies)
     uri = URI(url)
 
-    Net::HTTP.start(uri.host, uri.port, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE, open_timeout: 10, read_timeout: 600) do |http|
+    Net::HTTP.start(uri.host, uri.port, use_ssl: true, open_timeout: 10, read_timeout: 600) do |http|
       request = Net::HTTP::Get.new(uri)
       response = http.request(request)
 
